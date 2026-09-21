0cee36078ef60ec7d0a77a6322bdb3a21bc3ab03
Hamim Hossain <Hamim404@proton.me>
2026-09-21 09:04:54 +0600

    Project Structuring Complete

---
+++ src/config/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrHmsTWoDxFFQxr0LmUgqlDNCFgE5SIao",
+  authDomain: "smart-deals-23aaf.firebaseapp.com",
+  projectId: "smart-deals-23aaf",
+  storageBucket: "smart-deals-23aaf.firebasestorage.app",
+  messagingSenderId: "654947676357",
+  appId: "1:654947676357:web:21bcd43fc6a36d406fc0ac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
